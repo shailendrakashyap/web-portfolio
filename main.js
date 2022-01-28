d9b61b2fc04fadff1b6ab742758edeb6f0225287
Siwon  Kim <siwon@usf.edu>
2022-01-27 20:44:29 -0500

    JS: button click event

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict'
 
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
@@ -10,3 +11,18 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+// Handle scrolling whentapping on the navbar menu and contact button
+const navbarMenu = document.querySelector('.navbar__menu');
+const contactBtn = document.querySelector('.home__contact');
+[navbarMenu, contactBtn].forEach((clickEvent)=>{
+    clickEvent.addEventListener('click', (event) => {
+        const target = event.target;
+        const link = target.dataset.link;
+        if(link == null) {
+            return
+        }
+        const scrollTo = document.querySelector(link);
+        scrollTo.scrollIntoView({behavior: "smooth"});
+    });
+})
