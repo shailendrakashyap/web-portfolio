f368643470454de0a892b633275587b9c1cc43ef
Siwon  Kim <siwon@usf.edu>
2022-01-27 23:05:55 -0500

    JS: goToTop button

--- main.js
+++ main.js
@@ -24,16 +24,35 @@ const contactBtn = document.querySelector('.home__contact');
         }
         scrollIntoView(link);
     });
-})
+});
+
+
+
+// fade out home page and scroll button slowly when scrolling down
+const home = document.querySelector('#home');
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - (window.scrollY / homeHeight);
+});
+
+// show scroll-btn when scrolling down
+const goToTopBtn = document.querySelector("#scroll-btn");
+
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight / 2) {
+        goToTopBtn.classList.add('visible');
+    } else {
+        goToTopBtn.classList.remove('visible');
+    }
+});
+
+// handle click on the scroll-btn 
+goToTopBtn.addEventListener('click', () => {
+    scrollIntoView('#home');
+});
 
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: "smooth"});
 }
-
-// fade out home page slowly when scrolling down
-const home = document.querySelector('#home');
-const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
-    home.style.opacity = 1 - (window.scrollY / homeHeight);
-})
